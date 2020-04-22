069d674d18e173514bac342b5905dd4d7bc05c2c
wzd <wzdwxjwxw@sjtu.edu.cn>
2020-04-22 16:15:40 +0800

    home基本配置，主页面配置

---
+++ home/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+from .models import Reward
+
+#  form是在处理上传数据的中转平台
---
+++ home/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+#  在这里建立连接链接
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
